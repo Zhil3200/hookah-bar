70f73cf3b5a00d73b7497c34c59c79172c576435
Evgeniy (laptop) <zhilkevich_212@mail.ru>
2024-05-15 02:24:05 +0300

    add slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,40 @@
-$('.burger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header__overlay').toggleClass('active');
-    $('.header__wrapper').toggleClass('active');
+
+
+$(document).ready(function(){
+    
+    $('.burger').on('click', function () {
+        $(this).toggleClass('active');
+        $('.header__overlay').toggleClass('active');
+        $('.header__wrapper').toggleClass('active');
+    });
+    
+    $('.masters__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        prevArrow: $('.masters__arrow.arr-prev'),
+        nextArrow: $('.masters__arrow.arr-next'),
+        appendDots: $('.masters__dots'),
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                dots: true,
+                slidesToShow: 1
+              }
+            }
+          ]
+        
+    });
+
+
 });
